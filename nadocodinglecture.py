c199bf29407d127be1d5d4b3294b93c684364153
wjdgusdh <anthonyjeong24@gmail.com>
2026-03-07 19:49:12 +0900

    new : 나도코딩 1분 문법 공부

---
+++ nadocodinglecture.py
@@ -0,0 +1,69 @@
+#lang
+
+#ang = 'abcdefg'
+
+#print(lang[0])
+
+#rint(lang[:3])
+
+
+#문자열 처리
+
+# snack = '삼결살'
+# two  =  '2인분'
+
+# juseyo = snack + two
+
+# juseyo += '주세요'
+
+
+# print(juseyo)
+
+
+#몇글자인지 구하기 
+
+# snack = '''안녕하세요
+# 저는동아대학교컴퓨터공학과 2학년
+# 23학번정현오라고합니다'''
+
+# print(len(snack))       
+
+#문자열 메소드?
+
+# letter = 'HoW ArE YoU'
+
+# print(letter.lower())
+
+# print (letter.upper())
+
+# print(letter.capitalize())
+
+# print(letter.title())
+
+# print(letter.swapcase())
+
+# print(letter.split())
+
+# print(letter.count('HoW'))
+
+# s='안녕하세요..'
+
+# print(s.strip('.'))
+
+# print(s.replace('안녕하세요','안녕'))
+
+# print(s.find("안녕"))
+# print(s.center(9,'-'))
+
+# 20분까지 들음 
+
+
+ 
+
+
+
+
+
+ 
+
+
