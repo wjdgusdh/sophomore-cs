9f051d7300334dd35ed43f66f18279fa422c1bce
wjdgusdh <anthonyjeong24@gmail.com>
2026-03-17 19:54:23 +0900

    new: 문제해결프로그래밍

---
+++ munjae.py
@@ -0,0 +1,66 @@
+# a = 2000
+# if a < 1000 :
+#     # print("100보다 작군요")
+
+# for i in range(1, 10, 1):
+#     print(i)
+
+# while True :
+#     print("ㅎ", end = " ")
+#     break
+# print("Bye")
+
+# def func1() :
+#     a = 100
+#     print(a)
+
+# def func2() :
+#     global a
+#     a = 200
+# a = 0
+
+# func1()
+# print(a)
+# func2()
+# print(a)
+
+# a = []
+# for i in range(0, 10):
+#     a.append(i)
+# print(len(a))
+# print(a)
+
+
+# a = [10, 50, 30, 40, 20]
+# print(a[0])
+# print(a[-1])
+# print(a[-4])
+# print(a[1:3])
+# print(a[2:])
+
+# a = [data for data in range(1, 20) if data % 4 == 0]
+# print(a)
+
+# def sumSquare(arr):
+#     result = 0
+#     for i in range(len(arr)):
+#         result += arr[i]**2
+#     return result
+# a=[1, 2, 3, 4]
+# print(sum(a))
+# print(sumSquare(a))
+# b=[1, 2, 1, 3, 2]
+# print(sum(b))
+# print(sumSquare(b))
+
+
+
+
+
+# d = { "정식":9500, "돈가스":9900 }
+
+# d["쌀국수"] = 10000   # 항목 추가
+# print(d)
+
+# d["돈가스"] = 11000   # 값 변경
+# print(d)
