4faf46e14fae1b11055d31e85760e1b3bdec2ef6
wjdgusdh <anthonyjeong24@gmail.com>
2026-03-18 13:07:41 +0900

    add: 알파벳 소문자 대문자 추가

--- gekchae.cpp
+++ gekchae.cpp
@@ -112,3 +112,15 @@
 //         return 0;
 
 // }
+
+// #include <iostream>
+// using namespace std;
+// int main(void){
+//     for(int i = 1; i <= 5; i++) {
+//         for(int j = 1; j <= i; j++) {
+//             cout << "*";
+
+//         }
+//         cout <<endl;
+//     }
+// }
